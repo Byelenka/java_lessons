8d906c88ce737878e90b1852b2a545c1908ef114
Byelenka <byelenka@gmail.com>
2018-10-16 23:49:52 +0300

    New functions added

--- sandbox/src/main/java/com/gmail/byelenka/MyFirstProgram.java
+++ sandbox/src/main/java/com/gmail/byelenka/MyFirstProgram.java
@@ -2,6 +2,25 @@ package com.gmail.byelenka;
 
 public class MyFirstProgram {
 	public static void main (String[] args) {
-		System.out.println ("Hello, world!");
+		hello("world");
+
+		double lenght = 5;
+        System.out.println("Площадь квадрата со стороной " + lenght + " равна " + area(lenght));
+
+        double a = 2;
+        double b = 3;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
 	}
+
+	public static void hello (String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+	public static double area (double l) {
+	    return l*l;
+    }
+
+    public static double area (double a, double b) {
+	    return a*b;
+    }
 }

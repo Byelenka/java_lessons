7fc2c13de7567935d422e85bb0ea7e971ce5eabf
Byelenka <byelenka@gmail.com>
2018-10-23 23:36:26 +0300

    ContactCreationTests+ApplicationManager+TestBase

--- addressbook-web-tests/src/test/java/com/gmail/byelenka/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/com/gmail/byelenka/addressbook/appmanager/ApplicationManager.java
@@ -1,5 +1,6 @@
 package com.gmail.byelenka.addressbook.appmanager;
 
+import com.gmail.byelenka.addressbook.ContactData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
@@ -44,5 +45,30 @@ public class ApplicationManager {
 
     public NavigationHelper getNavigationHelper() {
         return navigationHelper;
+    }
+
+    public void gotoHomePage() {
+      wd.findElement(By.linkText("home page")).click();
+    }
+
+    public void submitContactCreation() {
+      wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+
+    public void fillContactForm(ContactData contactData) {
+      wd.findElement(By.name("firstname")).clear();
+      wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+      wd.findElement(By.name("lastname")).clear();
+      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+      wd.findElement(By.name("address")).clear();
+      wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+      wd.findElement(By.name("email")).clear();
+      wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+      wd.findElement(By.name("home")).clear();
+      wd.findElement(By.name("home")).sendKeys(contactData.getHomenumber());
+    }
+
+    public void initContactCreation() {
+      wd.findElement(By.linkText("add new")).click();
     }
 }

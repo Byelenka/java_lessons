c5afd9ff7311bcfdd669ca4dd96a57511b460e24
Byelenka <byelenka@gmail.com>
2018-11-05 16:57:56 +0200

    Collections in GroupDeletionTests&GroupModificationTests

--- addressbook-web-tests/src/test/java/com/gmail/byelenka/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/gmail/byelenka/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,20 @@
 package com.gmail.byelenka.addressbook.tests;
 
 import com.gmail.byelenka.addressbook.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
       app.getNavigationHelper().gotoGroupPage();
+      List<GroupData> before = app.getGroupHelper().getGroupList();
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+      List<GroupData> after = app.getGroupHelper().getGroupList();
+      Assert.assertEquals(after.size(), before.size() + 1);
   }
 
 }

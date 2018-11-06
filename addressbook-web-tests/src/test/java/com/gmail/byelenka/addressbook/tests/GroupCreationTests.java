3e83ca1639523b477aec176171c05f7bb093a1c0
Lena Ptashkina <Byelenka@gmail.com>
2018-11-06 13:38:26 +0200

    Lambda (max Id)

--- addressbook-web-tests/src/test/java/com/gmail/byelenka/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/gmail/byelenka/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import com.gmail.byelenka.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -12,9 +13,14 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
       app.getNavigationHelper().gotoGroupPage();
       List<GroupData> before = app.getGroupHelper().getGroupList();
-      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+      GroupData group = new GroupData("test2", null, null);
+      app.getGroupHelper().createGroup(group);
       List<GroupData> after = app.getGroupHelper().getGroupList();
       Assert.assertEquals(after.size(), before.size() + 1);
+
+      group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+      before.add(group);
+      Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
 
 }

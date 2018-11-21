cc0dce339bad567730364400f03b3549d32057ce
Lena Ptashkina <Byelenka@gmail.com>
2018-11-21 16:35:19 +0200

    Reading from file in ContactCreationTests

--- addressbook-web-tests/src/test/java/com/gmail/byelenka/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/com/gmail/byelenka/addressbook/tests/ContactCreationTests.java
@@ -2,26 +2,63 @@ package com.gmail.byelenka.addressbook.tests;
 
 import com.gmail.byelenka.addressbook.model.ContactData;
 import com.gmail.byelenka.addressbook.model.Contacts;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
-      app.goTo().homePage();
-      Contacts before = app.contact().allCont();
-      File photo = new File("src/test/resources/SmehAva7.jpg");
-      ContactData contact = new ContactData()
-              .withFirstname("Leon").withLastname("Killer").withAddress("Paris").withEmail("killer@gmail.com").withHomeNumber("+123456789").withPhoto(photo).withGroup("[none]");
-      app.contact().createContact(contact);
-      Contacts after = app.contact().allCont();
-      assertThat(after.size(), equalTo(before.size() + 1));
-      assertThat(after, equalTo(
-              before.withAdded(contact.withId(after.stream().mapToInt((c)->c.getId()).max().getAsInt()))));
-  }
+    @DataProvider
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xStream = new XStream();
+        xStream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xStream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {}.getType()); //List<ContactData>.class
+        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validContactsFromJson")
+    public void testContactCreation(ContactData contact) throws Exception {
+        app.goTo().homePage();
+        Contacts before = app.contact().allCont();
+        //File photo = new File("src/test/resources/SmehAva7.jpg");
+        app.contact().createContact(contact);
+        Contacts after = app.contact().allCont();
+        assertThat(after.size(), equalTo(before.size() + 1));
+        assertThat(after, equalTo(
+                before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    }
 }

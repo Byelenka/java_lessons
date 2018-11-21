523b901504fc071b10a019c53d3bf486e19798f1
Lena Ptashkina <Byelenka@gmail.com>
2018-11-21 15:40:43 +0200

    ContactDataGenerator is created

--- addressbook-web-tests/src/test/java/com/gmail/byelenka/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/com/gmail/byelenka/addressbook/model/ContactData.java
@@ -1,20 +1,32 @@
 package com.gmail.byelenka.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.io.File;
 
+@XStreamAlias("contact")
 public class ContactData {
+    @XStreamOmitField
     private int id = Integer.MAX_VALUE;
+    @Expose
     private String firstname;
+    @Expose
     private String lastname;
+    @Expose
     private String address;
+    @Expose
     private String email;
     private String email2;
     private String email3;
     private String allEmails;
+    @Expose
     private String home;
     private String mobile;
     private String work;
     private String allPhones;
+    @Expose
     private String group;
     private File photo;
 

875b3bfb48d82ea2dff8548f8c1efb7d3c2e7199
Lena Ptashkina <Byelenka@gmail.com>
2018-12-04 18:29:02 +0200

    New module (Mantis) is added

---
+++ mantis-tests/src/test/java/com/gmail/byelenka/mantis/tests/TestBase.java
@@ -0,0 +1,23 @@
+package com.gmail.byelenka.mantis.tests;
+
+import com.gmail.byelenka.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+}

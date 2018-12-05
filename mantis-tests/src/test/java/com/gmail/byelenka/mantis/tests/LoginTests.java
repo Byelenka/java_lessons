8e6db7f21880fb5d5d4076e273ba7a018a00c323
Lena Ptashkina <Byelenka@gmail.com>
2018-12-05 19:16:42 +0200

    HttpSession is created

---
+++ mantis-tests/src/test/java/com/gmail/byelenka/mantis/tests/LoginTests.java
@@ -0,0 +1,18 @@
+package com.gmail.byelenka.mantis.tests;
+
+import com.gmail.byelenka.mantis.appmanager.HttpSession;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginTests extends TestBase {
+
+    @Test
+    public void testLogin() throws IOException {
+        HttpSession session = new app.newSession();
+        assertTrue(session.login("administrator", "root"));
+        assertTrue((session.isLoggedInAs("administrator")));
+    }
+}

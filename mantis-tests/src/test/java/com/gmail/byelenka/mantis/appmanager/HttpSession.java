c886a4a95631b773001143e7727f4920bf491c9a
Lena Ptashkina <Byelenka@gmail.com>
2018-12-06 10:40:48 +0200

    Correct HttpSession

--- mantis-tests/src/test/java/com/gmail/byelenka/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/com/gmail/byelenka/mantis/appmanager/HttpSession.java
@@ -12,7 +12,6 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,7 +25,7 @@ public class HttpSession {
     }
 
     public boolean login(String username, String password) throws IOException {
-        HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php");
+        HttpPost post = new HttpPost(app.getProperty("web.baseURL") + "/login.php");
         List<NameValuePair> params = new ArrayList<>();
         params.add(new BasicNameValuePair("username", username));
         params.add(new BasicNameValuePair("password", password));
@@ -47,7 +46,7 @@ public class HttpSession {
     }
 
     public boolean isLoggedInAs(String username) throws IOException {
-        HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
+        HttpGet get = new HttpGet(app.getProperty("web.baseURL") + "/index.php");
         CloseableHttpResponse response = httpclient.execute(get);
         String body = geTextFrom(response);
         return body.contains(String.format("<span class=\"italic\">%s</span>", username));

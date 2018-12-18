114118cc5771f451f888a386653e4b11bba8fc8c
Byelenka <byelenka@gmail.com>
2018-12-19 00:41:19 +0200

    RestTests updated

--- rest-sample/src/test/java/com/gmail/byelenka/rest/RestTests.java
+++ rest-sample/src/test/java/com/gmail/byelenka/rest/RestTests.java
@@ -1,6 +1,7 @@
 package com.gmail.byelenka.rest;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
@@ -8,21 +9,30 @@ import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.message.BasicNameValuePair;
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+import java.util.Iterator;
 import java.util.Set;
 
 public class RestTests {
 
     @Test
     public void testCreateIssue() throws IOException {
-        Set<Issue> oldIssues = getIssues();
-        Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
-        int issueId = createIssue(newIssue);
-        Set<Issue> newIssues = getIssues();
-        oldIssues.add(newIssue.withId(issueId));
-        Assert.assertEquals(newIssues, oldIssues);
+        if (isIssueOpen(631) == false) {
+            Set<Issue> oldIssues = getIssues();
+            Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
+            int issueId = createIssue(newIssue);
+            Set<Issue> newIssues = getIssues();
+            oldIssues.add(newIssue.withId(issueId));
+            Assert.assertEquals(newIssues, oldIssues);
+        } else {
+            skipIfNotFixed(631);
+        }
     }
 
     private Set<Issue> getIssues() throws IOException {
@@ -45,5 +55,34 @@ public class RestTests {
                 .returnContent().asString();
         JsonElement parsed = new JsonParser().parse(json);
         return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
+
+    public boolean isIssueOpen(int issueId) throws IOException {
+        String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues.json"))
+                .returnContent().asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        JsonArray issues = parsed.getAsJsonObject().get("issues").getAsJsonArray();
+        Set<Issue> allIssues = new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {
+        }.getType());
+        String statename = "";
+        Iterator<Issue> issueIter = allIssues.iterator();
+        while (issueIter.hasNext()) {
+            Issue tIssue = issueIter.next();
+            if (tIssue.getId() == issueId) {
+                statename = tIssue.getState_name();
+                break;
+            }
+        }
+        if (statename.equals("Closed")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void skipIfNotFixed(int issueId) throws IOException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
     }
 }
